0269e156bae8d505a5e13b7f0525dc546b93e2c7
RepikM <moriel4@outlook.com>
2018-11-29 12:33:51 -0800

    Mid term + assainment

---
+++ midterm/js/main.js
@@ -0,0 +1,7 @@
+function theFunction(){
+	if(document.getElementById('hide').style.display =='block'){
+		document.getElementById('hide').style.display ='none';
+	}else{
+		document.getElementById('hide').style.display ='block'
+	}
+}
